002624efe7f84ccb1ce18b5405fb790641f82878
yuqiang <1845871677@qq.com>
2019-03-26 09:12:45 +0800

    修改回復

--- ten_qa/src/main/java/org/ten_qa/dao/ReplyDao.java
+++ ten_qa/src/main/java/org/ten_qa/dao/ReplyDao.java
@@ -1,12 +1,8 @@
 package org.ten_qa.dao;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.ten_qa.pojo.Problem;
 import org.ten_qa.pojo.Reply;
 
 /**
@@ -14,15 +10,5 @@ import org.ten_qa.pojo.Reply;
  */
 @Repository
 public interface ReplyDao extends JpaRepository<Reply, String>, JpaSpecificationExecutor<Reply> {
-
-	/**
-	 * 根据标签ID查询最新问题列表
-	 * 
-	 * @param labelId
-	 * @param pageable
-	 * @return
-	 */
-	@Query("select p from Problem p where id in( select problemid from Pl	where labelid=?1 )	order by replytime desc")
-	public Page<Problem> findNewListByLabelId(String labelId, Pageable pageable);
 
 }
