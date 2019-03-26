5201742eaefbb112f1332b92b0d18c228719570e
yxq18 <yxq18@LAPTOP-CFM03LAC>
2019-03-26 22:50:20 +0800

    修该service

--- ten_base/src/main/java/org/ten_base/service/LabelService.java
+++ ten_base/src/main/java/org/ten_base/service/LabelService.java
@@ -14,10 +14,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.ten_base.dao.LabelDao;
 import org.ten_base.pojo.Label;
 import org.ten_common.util.IdWorker;
 
+@Transactional
 @Service
 public class LabelService {
 
