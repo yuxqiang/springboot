17654eaae9a10a4bcb212df87846202406d092f9
yuqiang <1845871677@qq.com>
2019-03-27 08:56:00 +0800

    修改组合主键

--- ten_qa/src/main/java/org/ten_qa/pojo/Pl.java
+++ ten_qa/src/main/java/org/ten_qa/pojo/Pl.java
@@ -4,13 +4,17 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_pl")
+@IdClass(PlPoJo.class)
 public class Pl implements Serializable {
+
 	@Id
 	private String problemid;
+
 	@Id
 	private String lableid;
 
