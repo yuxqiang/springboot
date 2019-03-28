12d735d16175615cd21130d8f7745827af927527
yuqiang <1845871677@qq.com>
2019-03-28 09:24:52 +0800

    新增招聘服务

---
+++ ten_gathering/src/main/java/org/ten_gathering/GatheringApplication.java
@@ -0,0 +1,21 @@
+package org.ten_gathering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.ten_common.util.IdWorker;
+
+@SpringBootApplication
+@EnableCaching
+public class GatheringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatheringApplication.class);
+	}
+
+	@Bean
+	public IdWorker idWorker() {
+		return new IdWorker(1, 1);
+	}
+}
